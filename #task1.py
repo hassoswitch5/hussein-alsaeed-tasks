ae0e3189c7588e02923a5a5f6c2fab839d2a5feb
hassoswitch5 <157315012+hassoswitch5@users.noreply.github.com>
2024-07-22 10:05:37 +0300

    task 3 variables

---
+++ #task1.py
@@ -0,0 +1,68 @@
+import math
+
+#task1                               )
+x = 6768
+# Convert the number to a string
+x_str = str(x)
+# Print each digit
+print(x_str[0])
+print(x_str[1])
+print(x_str[2])
+print(x_str[3])
+
+
+
+#task2
+
+      
+
+#define the variable
+x=5
+y=8
+
+#turn it into string
+str_x=str(x)
+str_y=str(y)
+
+#print it
+newnumber = str_x + str_y
+print(newnumber)
+
+
+#task3
+#r = 
+area = 33
+
+radius = math.sqrt(area/math.pi )
+
+circu = 2*math.pi*radius
+
+
+
+
+#task4
+
+name ='Hussein'
+print(name[0:6:2]) #[start:end+1:steps]
+
+#task5
+
+name = 'Hussein'
+
+Cap = name.capitalize()  
+print(Cap[:2])  
+print(Cap[:2])  
+print(Cap[:2])  
+
+#task6
+txt = "I love myself"
+txt2= "myself"
+txt3= "protons"
+x = txt.replace(txt2, txt3)
+
+print(x)
+
+
+
+
+
